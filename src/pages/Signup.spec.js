71b125f714a0d3e739b6b9a60244c73b97a083e5
Mt-Lampert <perrin.blog@web.de>
2022-06-21 08:15:36 +0200

    p: Refactoring => Make Rendering DRY.

--- src/pages/Signup.spec.js
+++ src/pages/Signup.spec.js
@@ -2,21 +2,26 @@ import { render, screen } from "@testing-library/react"
 import Signup from "./Signup"
 
 describe("Signup Component", () => {
+
+  beforeEach(() => {
+    render(<Signup/>)
+  })
+
   describe("on HTML level", () => {
     it("has a header", () => {
-      render(<Signup></Signup>)
+      // render(<Signup></Signup>)
       const header = screen.getByText("Please Sign up!")
       expect(header).toBeInTheDocument()
     })
 
     it("has a user name input field", () => {
-      render(<Signup />)
+      // render(<Signup />)
       const nameInput = screen.getByPlaceholderText("Enter your username")
       expect(nameInput).toBeInTheDocument
     })
 
     it("has an email input field", () => {
-      render(<Signup />)
+      // render(<Signup />)
       const mailInput = screen.getByPlaceholderText("Enter your email address")
       expect(mailInput).toBeInTheDocument
     })
