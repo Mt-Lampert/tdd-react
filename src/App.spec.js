81a7fbb2e29abca60c5d17c049cf2ab681152fe3
Mt-Lampert <perrin.blog@web.de>
2022-07-13 13:02:52 +0200

    w: adding tests for dynamic  '/user' routes

--- src/App.spec.js
+++ src/App.spec.js
@@ -6,15 +6,20 @@ describe("Routing", () => {
     { path: "/", testID: "home-page" },
     { path: "/signup", testID: "signup-page" },
     { path: "/login", testID: "login-page" },
-    { path: "/user/1", testID: "user-page"},
-    { path: "/user/22", testID: "user-page"},
+    { path: "/user/1", testID: "user-page" },
+    { path: "/user/22", testID: "user-page" },
   ];
   const nullRoutes = [
     { path: "/", testID: "signup-page" },
     { path: "/signup", testID: "home-page" },
     { path: "/login", testID: "home-page" },
-    { path: "/user/22", testID: "home-page"},
-    { path: "/user/22", testID: "landing-page"},
+    { path: "/user/22", testID: "home-page" },
+    { path: "/user/22", testID: "landing-page" },
+  ];
+
+  const navbarLinks = [
+    { path: "/", roleName: "Home" },
+    { path: "/signup", roleName: "Signup" },
   ];
 
   it.each(routes)(
@@ -34,6 +39,16 @@ describe("Routing", () => {
       render(<App />);
       const myComponent = screen.queryByTestId(testID);
       expect(myComponent).not.toBeInTheDocument();
+    }
+  );
+
+  it.each(navbarLinks)(
+    "has link to '$roleName' on the navbar",
+    ({ path, roleName }) => {
+      window.history.pushState({}, "", path);
+      render(<App />);
+      const link = screen.getByRole("link", { name: roleName });
+      expect(link).toBeInTheDocument();
     }
   );
 
