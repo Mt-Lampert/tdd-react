2bf205467a0bcd2caa576d0f8cf7de2ff9bc7834
Mt-Lampert <perrin.blog@web.de>
2022-07-14 17:19:24 +0200

    w: formatting Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,28 @@
-
-export default function Navbar({updateEndpoint}) {
-
+export default function Navbar({ updateEndpoint }) {
   function linkHandler(e) {
     e.preventDefault();
-    updateEndpoint(e.target.attributes.href.value)
+    updateEndpoint(e.currentTarget.attributes.href.value);
   }
 
   return (
-    <div>
-      <a href="/" title="Home" onClick={linkHandler}>Hoaxify</a> &nbsp; 
-      <a href="/signup" onClick={linkHandler} >Signup</a> &nbsp;
-      <a href="/login" onClick={linkHandler}>Login</a>
-    </div>
-  )
-};
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <a href="/" className="navbar-link" title="Home" onClick={linkHandler}>
+          <img src="/hoaxify.png" alt="Logo" width="20%"/>
+          <b>Hoaxify</b>
+        </a>
+      </div>
+      <div className="navbar-menu">
+        <div className="navbar-end">
+          <a href="/signup" className="navbar-link" onClick={linkHandler}>
+            Signup
+          </a>
+
+          <a href="/login" className="navbar-link" onClick={linkHandler}>
+            Login
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
